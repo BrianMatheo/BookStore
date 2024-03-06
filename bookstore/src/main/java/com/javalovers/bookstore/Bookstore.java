a7027841b61bb3f8e2a002628c0a6d57ff361bb2
Brian Matheo <bmalvarezp@ufpso.edu.co>
2024-03-06 14:44:10 -0500

    Creación de la BookStore

---
+++ bookstore/src/main/java/com/javalovers/bookstore/Bookstore.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.javalovers.bookstore;
+
+/**
+ *
+ * @author SCIS2PC14
+ */
+public class Bookstore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
